6e0f7cb2c0299c39fbe07f37436457ea2f9bb399
Collins Jumah <collinsjumah@cabinda.local>
2023-09-24 20:26:59 +0300

    feat: Sveltkit learning

---
+++ src/components/Authenticate.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+
+let email=""
+let password=""
+let confirpassword=""
+let error=false
+let register = false
+
+function handleAuthentication(){
+    if(!email || !password || (register && ! confirpassword)){
+        error= true
+        return
+    }
+}
+</script>
+
+<div class="authContainer">
+	<form action="" autocomplete="off">
+		<h1>{ register? 'Register' : 'Login'}</h1>
+        {#if error}
+            <p class="error">Information entered is incorrect. Please try again!</p>
+        {/if}
+		<label for="email">
+            <p class={email ? "above" : "center"}>Email</p>
+			<input bind:value={email} type="email" name="email" id="" placeholder="Enter your email address"/>
+		</label>
+		<label for="password">
+            <p class={password ? "above" : "center"}>password</p>
+			<input bind:value={password} type="password" name="email" id="" placeholder="Enter your password"/>
+		</label>
+		<label for="password">
+            <p class={confirpassword ? "above" : "center"}>password again</p>
+			<input bind:value={confirpassword} name="confirm-password" type="password" id="" placeholder="Password again"/>
+		</label>
+        <button type="button">Submit</button>
+	</form>
+</div>
+
+<style>
+	.authContainer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+        justify-content: center;
+        flex: 1;
+        transition-duration: 200ms;
+	}
+    h1{
+        text-align: center;
+        font-size: 3rem;
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        gap: 14px;
+        width: 400px;
+        max-width: 100%;
+        margin: 0 auto;
+        padding: 24px;
+    }
+    form label{
+        position: relative;
+        border: 1px solid rgb(62, 62, 210);
+        border-radius: 5px;
+    }
+    form label:focus-within{
+        border-color: blue;
+    }
+    form input{
+        width: 100%;
+        padding: 14px;
+        border: none;
+        background: transparent;
+        color: white;
+    }
+    form input:focus{
+        border: none;
+        outline: none;
+    }
+    form button{
+        background-color: blue;
+        color: white;
+        border: none;
+        padding: 10px 0;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+    form button:hover{
+        background-color: transparent;
+        animation-duration: 300ms;
+        animation-delay: 500ms;
+        border: 1px solid blue;
+
+    }
+   .above, .center{
+    position: absolute;
+    transform: translateY(-50%);
+    pointer-events: none;
+    color: white;
+    border-radius: 5px;
+    padding: 0 6px;
+    font-size: 0.8rem;
+   }
+   .above{
+    top: 0;
+    left:15px;
+    background-color: blue;
+    border: 1px solid blue;
+    font-size: 0.8rem;
+   }
+   .center{
+    top: 50%;
+    left: 6px;
+    border: 1px solid transparent;
+    opacity: 0;
+   }
+   .error{
+    color: coral;
+    font-size: 0.9rem;
+   }
+   </style>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	let symptoms = ['Headache','another one','and another'];
+    let currentSymptom =''
+    let error=false
+    let addSymptom = () =>{
+        if(!currentSymptom){
+            error=true
+        }
+        symptoms=[...symptoms,currentSymptom]
+        currentSymptom=''
+    }
+</script>
+
+<div class="mainContainer">
+	<div class="header-container">
+		<h1>Add Symptoms</h1>
+		<button>save</button>
+	</div>
+	<main>
+        {#each symptoms as todo,index}
+            <div class="todo">
+                {index+1}.{todo}
+            </div>
+        {/each}
+    </main>
+	<div class={"enterToDo" + (error ? "errorBorder" :" ")}>
+		<input bind:value={currentSymptom} type="text" placeholder="Enter Symptoms" id="" />
+		<button on:click={addSymptom}>ADD</button>
+	</div>
+</div>
+
+<style>
+	.mainContainer {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+		gap: 24px;
+		padding: 24px;
+		width: 100%;
+		max-width: 1000px;
+		margin: 0 auto;
+	}
+	.header-container {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.header-container button {
+		background-color: #003c5b;
+		color: white;
+		padding: 10px 12px;
+		border: none;
+		border-radius: 5px;
+		cursor: pointer;
+	}
+	main {
+		display: flex;
+		flex-direction: column;
+		gap: 8px;
+		flex: 1;
+	}
+	.enterToDo {
+		display: flex;
+		align-items: stretch;
+		border: 1px solid white;
+	}
+	.enterToDo input {
+		background: transparent;
+		border: none;
+		padding: 14px;
+		color: white;
+		flex: 1;
+	}
+	.enterToDo input:focus {
+		outline: none;
+	}
+	.enterToDo button {
+		padding: 0 14px;
+		background-color: #003c5b;
+		border: none;
+		color: white;
+		font-weight: 600;
+		cursor: pointer;
+	}
+	.enterToDo button:hover {
+		background: transparent;
+	}
+</style>
